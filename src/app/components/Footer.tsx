e854bf031a4cd13b809e90859583f5eb3e8fd901
Esora Kyugo <tom.7trouillet@gmail.com>
2025-07-25 19:37:00 +0200

    getting some changes
    modification of the todo converter

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,17 @@
 import styles from "./Footer.module.scss";
 
+/**
+ * Footer component that renders the application's footer section.
+ * Displays a text indicating the director or creator.
+ *
+ * @returns {JSX.Element} The rendered footer element.
+ */
 export default function Footer() {
   return (
     <footer>
       <p className={styles.textfooter}>dierected by esora</p>
     </footer>
   );
+
+  
 }
--- src/database/converter/TodoConverter.ts
+++ src/database/converter/TodoConverter.ts
@@ -17,3 +17,37 @@ export default class TodoConverter {
     };
   }
 }
+
+import { ToDoDTO, ToDoDTODataBase } from "@/types/todo";
+export function toCoreDTO(dataBaseDTO: ToDoDTODataBase): ToDoDTO {
+  return {
+    id: dataBaseDTO.id.toString(),
+    tache: dataBaseDTO.tache,
+  };
+}
+export function toDataBaseDTO(coreDTO: ToDoDTO): ToDoDTODataBase {
+  return {
+    id: BigInt(coreDTO.id),
+    tache: coreDTO.tache,
+  };
+}
+export function toCoreDTOList(dataBaseDTOList: ToDoDTODataBase[]): ToDoDTO[] {
+  return dataBaseDTOList.map(toCoreDTO);
+}
+export function toDataBaseDTOList(coreDTOList: ToDoDTO[]): ToDoDTOData
+Base[] {
+  return coreDTOList.map(toDataBaseDTO);
+}
+export function toCoreEntityList(dataBaseEntityList: TodoEntity[]): Todo[] {
+  return dataBaseEntityList.map((entity) => TodoConverter.toCoreEntity(entity));
+}
+export function toDataBaseEntityList(coreEntityList: Todo[]): TodoEntity[] {
+  return coreEntityList.map((entity) => TodoConverter.toDataBaseEntity(entity));
+}
+export function toCoreDTOFromEntity(dataBaseEntity: TodoEntity): ToDoDTO {
+  const coreEntity = TodoConverter.toCoreEntity(dataBaseEntity);
+  return {
+    id: coreEntity.getId().toString(),
+    tache: coreEntity.getTache(),
+  };
+}
